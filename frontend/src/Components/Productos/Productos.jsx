8b60990d48a5bb8b761f037d8126e0d8af402e53
Anthonella Rua <anthonella.rua@tecsup.edu.pe>
2022-10-13 23:41:03 -0500

    terminado

--- frontend/src/Components/Productos/Productos.jsx
+++ frontend/src/Components/Productos/Productos.jsx
@@ -69,16 +69,19 @@ class Productos extends Component {
             <div className="container">
                 <div className="row row-cols-1 row-cols-md-4 g-4">  {
                 items.map((item) => ( 
-                    <div className="card me-5">
-                    <img className="card-img-top" src={item.img} alt={item.id}/>
-                    <div className="card-body">
-                        <h5 className="card-title">{item.nombre}</h5>
-                        <p className="card-text">S/. {item.precio}</p>
-                        <a className="btn btn-primary">Añadir al carrito</a>
-                    </div>
-                    <div className="card-footer">
-                    <small className="text-muted">{item.categoria}</small>
-                    </div>
+                    <div class="col">
+                        <div className="card me-5">
+                            <img className="card-img-top" src={item.img} alt={item.id}/>
+                            <div className="card-body">
+                                <h5 className="card-title">{item.nombre}</h5>
+                                <p class="card-text">{item.descripcion}</p>
+                                <p className="card-text">S/. {item.precio}</p>
+                                <a className="btn btn-primary">Añadir al carrito</a>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">{item.categoria}</small>
+                            </div>
+                        </div>
                     </div>
                 ))
                 }
